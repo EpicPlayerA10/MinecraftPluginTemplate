2608070a157c7c2a0a8f30808025662d23ce6f0e
EpicPlayerA10 <adasko.lesiak@gmail.com>
2024-07-18 14:35:18 +0200

    Initial commit

---
+++ src/main/java/com/epicplayera10/exampleplugin/commands/ExampleCommand.java
@@ -0,0 +1,10 @@
+package com.epicplayera10.exampleplugin.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+
+@CommandAlias("teams")
+@CommandPermission("exampleplugin.examplecommand")
+public class ExampleCommand extends BaseCommand {
+}
