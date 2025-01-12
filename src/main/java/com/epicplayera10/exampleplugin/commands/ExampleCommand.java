840c9eece9d24d0660d87c9700a71ab953c4818f
EpicPlayerA10 <62206933+EpicPlayerA10@users.noreply.github.com>
2025-01-12 16:48:56 +0100

    Update ExampleCommand.java

--- src/main/java/com/epicplayera10/exampleplugin/commands/ExampleCommand.java
+++ src/main/java/com/epicplayera10/exampleplugin/commands/ExampleCommand.java
@@ -1,8 +1,10 @@
 package com.epicplayera10.exampleplugin.commands;
 
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.HelpCommand;
+import org.bukkit.command.CommandSender;
 
 @CommandAlias("examplecommand")
 @CommandPermission("exampleplugin.examplecommand")
