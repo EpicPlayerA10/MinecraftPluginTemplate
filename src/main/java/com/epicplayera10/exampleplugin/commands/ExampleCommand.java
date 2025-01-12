c9a1ccca7098fc93d80ad48172c925af7a5094d9
EpicPlayerA10 <62206933+EpicPlayerA10@users.noreply.github.com>
2025-01-12 16:36:41 +0100

    Update ExampleCommand.java

--- src/main/java/com/epicplayera10/exampleplugin/commands/ExampleCommand.java
+++ src/main/java/com/epicplayera10/exampleplugin/commands/ExampleCommand.java
@@ -4,7 +4,7 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 
-@CommandAlias("teams")
+@CommandAlias("examplecommand")
 @CommandPermission("exampleplugin.examplecommand")
 public class ExampleCommand extends BaseCommand {
 }
